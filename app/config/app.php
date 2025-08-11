5496cbd37eef45854b40d3feaa337fb291faa794
Shivam <shivammittalsm18@gmail.com>
2025-08-11 19:09:33 +0530

    update: encoding to utf8

--- app/config/app.php
+++ app/config/app.php
@@ -288,7 +288,7 @@ return [
             /*
              * For MariaDB/MySQL the internal default changed from utf8 to utf8mb4, aka full utf-8 support
              */
-            'encoding' => 'utf8mb4',
+            'encoding' => 'utf8',
 
             /*
              * If your MySQL server is configured with `skip-character-set-client-handshake`
